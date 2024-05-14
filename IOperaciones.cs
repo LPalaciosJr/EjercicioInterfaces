8e41c8c7d2832cf9f9a3ed6bb05d6d0b01712e1c
Luis Palacios <lpalacios1119@gmail.com>
2024-05-13 23:25:03 -0600

    Se creo la interface

---
+++ IOperaciones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioInterfaces
+{
+    public interface IOperaciones
+    {
+        double CalcularAreaCirculo(double radio);
+        double CalcularAreaRectangulo(double largo, double ancho);
+        double CalcularVolumenCubo(double lado);
+        double CalcularLongitudCircunferencia(double radio);
+        double CalcularVolumenEsfera(double radio);
+    }
+}

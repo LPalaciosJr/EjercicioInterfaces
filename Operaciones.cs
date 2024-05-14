4419a5e823531bc2b880b6bae384092edc408edd
Luis Palacios <lpalacios1119@gmail.com>
2024-05-13 23:47:15 -0600

    Clase implementando interfaz y metodo de imprimir en program.cs

---
+++ Operaciones.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioInterfaces
+{
+    public class Operaciones : IOperaciones
+    {
+        public double CalcularAreaCirculo(double radio)
+        {
+            return Math.PI * radio * radio;
+        }
+
+        public double CalcularAreaRectangulo(double largo, double ancho)
+        {
+            return largo * ancho;
+        }
+
+        public double CalcularLongitudCircunferencia(double radio)
+        {
+            return 2 * Math.PI * radio;
+        }
+
+        public double CalcularVolumenCubo(double lado)
+        {
+            return Math.Pow(lado, 3);
+        }
+
+        public double CalcularVolumenEsfera(double radio)
+        {
+            return(4.0 / 3.0) * Math.PI * Math.Pow(radio, 3);
+        }
+
+        public void Imprimir(double radioCirculo, double largoRectangulo, double anchoRectangulo, double ladoCubo, double radioEsfera)
+        {
+            Console.WriteLine("Resultados de las Operaciones:");
+            Console.WriteLine($"Área del círculo (radio {radioCirculo}): {CalcularAreaCirculo(radioCirculo)}");
+            Console.WriteLine($"Área del rectángulo (largo {largoRectangulo}, ancho {anchoRectangulo}): {CalcularAreaRectangulo(largoRectangulo, anchoRectangulo)}");
+            Console.WriteLine($"Volumen del cubo (lado {ladoCubo}): {CalcularVolumenCubo(ladoCubo)}");
+            Console.WriteLine($"Longitud de la circunferencia (radio {radioCirculo}): {CalcularLongitudCircunferencia(radioCirculo)}");
+            Console.WriteLine($"Volumen de la esfera (radio {radioEsfera}): {CalcularVolumenEsfera(radioEsfera)}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EjercicioInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones operaciones = new Operaciones();
+
+            Console.WriteLine("Ingrese el radio del círculo:");
+            double radioCirculo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el largo del rectángulo:");
+            double largoRectangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el ancho del rectángulo:");
+            double anchoRectangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el lado del cubo:");
+            double ladoCubo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el radio de la esfera:");
+            double radioEsfera = Convert.ToDouble(Console.ReadLine());
+
+            operaciones.Imprimir(radioCirculo, largoRectangulo, anchoRectangulo, ladoCubo, radioEsfera);
+        }
+    }
+}
